493906c00d4c8528601f14f04c25915aeb42b7dc
Trinad <thrinadhrao@lotuswireless.com>
2021-12-06 18:27:38 +0530

    Changes#1

---
+++ src/redux-toolkit.ts
@@ -0,0 +1,76 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { v1 as uuid } from "uuid";
+import { Todo } from "./type";
+
+const todosInitialState: Todo[] = [
+  {
+    id: uuid(),
+    desc: "Learn React",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    desc: "Learn Redux",
+    isComplete: true,
+  },
+  {
+    id: uuid(),
+    desc: "Learn Redux-ToolKit",
+    isComplete: false,
+  },
+];
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: todosInitialState,
+  reducers: {
+    create: {
+      reducer: (
+        state,
+        {
+          payload,
+        }: PayloadAction<{ id: string; desc: string; isComplete: boolean }>
+      ) => {
+        state.push(payload);
+      },
+      prepare: ({ desc }: { desc: string }) => ({
+        payload: {
+          id: uuid(),
+          desc,
+          isComplete: false,
+        },
+      }),
+    },
+    edit: (state, { payload }: PayloadAction<{ id: string; desc: string }>) => {
+      const todoToEdit = state.find((todo) => todo.id === payload.id);
+      if (todoToEdit) {
+        todoToEdit.desc = payload.desc;
+      }
+    },
+    toggle: (
+      state,
+      { payload }: PayloadAction<{ id: string; isComplete: boolean }>
+    ) => {
+      const todoToToggle = state.find((todo) => todo.id === payload.id);
+      if (todoToToggle) {
+        todoToToggle.isComplete = payload.isComplete;
+      }
+    },
+    remove: (state, { payload }: PayloadAction<{ id: string }>) => {
+      const index = state.findIndex((todo) => todo.id === payload.id);
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
+    },
+  },
+});
+
+const selectedTodoSlice = createSlice({
+    name: 'selectedTodo',
+    initialState: null as string | null,
+    reducers: {
+        select: (state, {payload}: PayloadAction<>) => {
+
+        }
+    }
+})
